c7b687a028419aa43163218f0f2a54dd4f5a61b2
Young Bin Na <whybee0405@gmail.com>
2023-12-20 15:16:25 +0200

    finalize implementation of sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'erp_system.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ERP_project.settings'
 django.setup()
 
 # Configuration file for the Sphinx documentation builder.
